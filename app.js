2327255eadc0d13f3ba46407327f0060a427c4b8
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2021-07-13 12:28:29 +0600

    new file created

---
+++ app.js
@@ -0,0 +1,24 @@
+document.write("this is guessing game<br><br>");
+
+
+var numOfWon = 0;
+var numOfLost = 0;
+
+for(var i = 1; i<=5; i++){
+    var num = parseFloat(prompt("Enter your number from 1 to 5"));
+
+    var randNum = Math.floor(Math.random()*5) + 1;
+
+    if(num == randNum){
+        document.write("you have won<br>");
+        numOfWon++;
+    }
+    else{
+        document.write("you have lost. Random number was " + randNum +"<br>");
+        numOfLost++;
+    }
+}
+
+document.write("<br>");
+document.write("You have won " + numOfWon + " times<br>");
+document.write("You have lost " + numOfLost + " times");
